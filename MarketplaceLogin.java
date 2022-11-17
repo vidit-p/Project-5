2b350e9928c8f4125dc8be8bfdd9f1adf720f055
EJPotter8 <112223564+EJPotter8@users.noreply.github.com>
2022-11-17 12:57:40 -0500

    Update MarketplaceLogin.java

--- MarketplaceLogin.java
+++ MarketplaceLogin.java
@@ -1,22 +1,15 @@
 import org.w3c.dom.ls.LSOutput;
 
+import javax.swing.*;
 import java.io.*;
 import java.util.ArrayList;
 import java.util.Scanner;
 
-/**
- * Main marketplace
- *
- *
- *
- * @author Vidit Patel
- * @version November 14, 2022
- */
-public class MarketplaceLogin {
+public class Marketplace {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        String a;
+        int ans;
         String b;
         String sellerOrCustomer;
         String username;
@@ -26,29 +19,25 @@ public class MarketplaceLogin {
         String subLine;
         String realUsername = " ";
 
-        System.out.println("Welcome to the digital marketplace!");
-
+        JOptionPane.showMessageDialog(null, "Welcome to the digital marketplace!", "Marketplace", JOptionPane.INFORMATION_MESSAGE);
         do {
-            System.out.println("Do you currently have an account?");
-            System.out.println("1 = Yes, 2 = No");
 
-            a = scanner.nextLine();
+            ans = JOptionPane.showConfirmDialog(null, "Do you currently have an account?", "Marketplace", JOptionPane.YES_NO_OPTION);
 
-            if (a.equals("1")) {
+            if (ans == JOptionPane.YES_OPTION) {
                 do {
-                    System.out.println("Please enter your email or username:");
-                    username = scanner.nextLine();
+                    username = JOptionPane.showInputDialog(null, "Please enter your email or username:", "Marketplace", JOptionPane.INFORMATION_MESSAGE);
                     try {
                         BufferedReader bufferedReader = new BufferedReader(new FileReader("accounts.txt"));
                         for (int i = 0; i < 1; i++) {
                             while ((line = bufferedReader.readLine()) != null) {
                                 realUsername = line.substring(line.indexOf(' ') + 1, line.indexOf('_'));
                                 if (realUsername.equals(username)) {
+                                    JOptionPane.showMessageDialog(null, "Username exists.", "Marketplace", JOptionPane.INFORMATION_MESSAGE);
                                     do {
-                                        System.out.println("Please enter your password:");
-                                        password = scanner.nextLine();
+                                        password = JOptionPane.showInputDialog(null, "Please enter your password:", "Marketplace", JOptionPane.INFORMATION_MESSAGE);
                                         if (line.substring(line.indexOf('_') + 1).equals(password)) {
-                                            System.out.println("You are now logged in to your account!");
+                                            JOptionPane.showMessageDialog(null, "You are now logged in to your account!", "Marketplace", JOptionPane.INFORMATION_MESSAGE);
                                             if (line.charAt(0) == '1') {
                                                 Seller seller = new Seller(username, password);
                                                 seller.initialise();
@@ -511,22 +500,28 @@ public class MarketplaceLogin {
                                                 }
                                             }
                                         } else {
-                                            System.out.println("Incorrect password! Please try again.");
+                                                        JOptionPane.showMessageDialog(null, "Invalid input! Please try again.", "Marketplace", JOptionPane.INFORMATION_MESSAGE);
+
+                                                    }
+                                                }
+                                            }
+                                        } else {
+                                            JOptionPane.showMessageDialog(null, "Incorrect password! Please try again.", "Marketplace", JOptionPane.INFORMATION_MESSAGE);
                                         }
                                     } while (!line.substring(line.indexOf('_') + 1).equals(password));
                                 }
                             }
-                            System.out.println("There is no current account with this username. Try again.");
+                            JOptionPane.showMessageDialog(null, "There is no current account with this username. Try again.", "Marketplace", JOptionPane.INFORMATION_MESSAGE);
                             if (!realUsername.equals(username)) {
                                 break;
                             }
                         }
                     } catch (Exception e) {
-                        e.printStackTrace();
+
                     }
                 } while (!username.equals(realUsername));
 
-            } else if (a.equals("2")) {
+            } else if (ans == JOptionPane.NO_OPTION) {
                 do {
                     System.out.println("Would you like to create an account?");
                     System.out.println("1 = Yes, 2 = No");
