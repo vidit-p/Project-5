95ea6a5242fdf2f1d5d36b327e162e4773e48fcb
EJPotter8 <112223564+EJPotter8@users.noreply.github.com>
2022-11-17 12:57:51 -0500

    Update MarketplaceLogin.java

--- MarketplaceLogin.java
+++ MarketplaceLogin.java
@@ -5,7 +5,7 @@ import java.io.*;
 import java.util.ArrayList;
 import java.util.Scanner;
 
-public class Marketplace {
+public class MarketplaceLogin {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
